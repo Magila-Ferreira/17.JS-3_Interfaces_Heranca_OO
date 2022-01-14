33b56b88c44270b5827f3e8da607ba721d0b379d
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-01-14 10:50:42 -0300

    14/01/22_Aula 1 - Conhecendo o problema do cliente

---
+++ Conta.js
@@ -0,0 +1,22 @@
+export class Conta {
+    sacar(valor) {
+        let taxa = 1.1;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
---
+++ ContaPoupanca.js
@@ -0,0 +1,27 @@
+export class ContaPoupanca {
+    constructor(agencia, cliente, saldoInicial) {
+        this._agencia = agencia;
+        this._cliente = cliente;
+        this._saldo = saldoInicial;
+    }
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
